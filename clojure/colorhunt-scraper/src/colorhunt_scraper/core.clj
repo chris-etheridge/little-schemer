caf4dacfa89261f91bb9ad99c7d751ea18ab08f6
Chris Etheridge <chris.etheridge@cognician.com>
2016-05-21 22:18:20 +0200

    Scraped data

--- clojure/colorhunt-scraper/src/colorhunt_scraper/core.clj
+++ clojure/colorhunt-scraper/src/colorhunt_scraper/core.clj
@@ -1,10 +1,12 @@
 (ns colorhunt-scraper.core
   (:require [clojure.string :as string]))
 
+;; this is far too imperative
 (def data (string/split (slurp "resources/data.txt") #","))
 
 (def clean-data (map #(string/replace % #"'" "") data))
 
+;; this is super ugly code
 (def colors (map (fn [s]
                    [(str "#" (subs s 0 6))
                     (str "#" (subs s 6 12))
