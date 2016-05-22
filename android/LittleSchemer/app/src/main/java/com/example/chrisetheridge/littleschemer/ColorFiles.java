77c365b7e5dcb34563f39cad55013de33866c2b4
Chris Etheridge <chris.etheridge@cognician.com>
2016-05-22 11:50:33 +0200

    Added init file

--- android/LittleSchemer/app/src/main/java/com/example/chrisetheridge/littleschemer/ColorFiles.java
+++ android/LittleSchemer/app/src/main/java/com/example/chrisetheridge/littleschemer/ColorFiles.java
@@ -1,6 +1,13 @@
 package com.example.chrisetheridge.littleschemer;
 
+import android.content.Context;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by chrisetheridge on 5/22/16.
@@ -9,13 +16,33 @@ public class ColorFiles {
 
     public static class Util {
 
+        private static BufferedReader bufr;
+
         // main method which loads our data from a file PATH
-        public static void loadData(String path) {}
+        // also parses the data with a delimiter
+        public static List<String> loadData(String path, String deli, Context ctx) {
+            File f = new File(path);
+            List<String> cs = Arrays.asList("");
+
+            try {
+                bufr = new BufferedReader(new FileReader(path));
+                String line = bufr.readLine()
+
+                while(line != null) {
+
+                }
+
+            }
+
+            catch (IOException e) {}
+
+            return cs;
+        }
 
         // parses a single line from a data blob
         // requires a delimiter to be provided
         private static String[] parseLine(String line, String deli) {
-            return new String[3];
+            return line.split(deli);
         }
 
     }
