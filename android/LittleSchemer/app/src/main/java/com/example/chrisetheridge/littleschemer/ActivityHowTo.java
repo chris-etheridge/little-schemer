73e8a653c7e325b1890a09984ef01f81b04e87aa
Chris Etheridge <chris.etheridge@cognician.com>
2016-05-22 20:07:33 +0200

    Added how to activity scaffold

---
+++ android/LittleSchemer/app/src/main/java/com/example/chrisetheridge/littleschemer/ActivityHowTo.java
@@ -0,0 +1,13 @@
+package com.example.chrisetheridge.littleschemer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityHowTo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_how_to);
+    }
+}
