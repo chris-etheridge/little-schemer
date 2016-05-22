47829186e64b6f47cc3701095baa494435c270cb
Chris Etheridge <chris.etheridge@cognician.com>
2016-05-22 20:42:38 +0200

    Hooked up buttons on main menu and how to screen

--- android/LittleSchemer/app/src/main/java/com/example/chrisetheridge/littleschemer/ActivityHowTo.java
+++ android/LittleSchemer/app/src/main/java/com/example/chrisetheridge/littleschemer/ActivityHowTo.java
@@ -1,7 +1,9 @@
 package com.example.chrisetheridge.littleschemer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityHowTo extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class ActivityHowTo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_how_to);
+    }
+
+    public void okayButtonOnTap(View view) {
+        Intent i = new Intent(this, LittleSchemerMain_v2.class);
+
+        startActivity(i);
+
+        finish();
     }
 }
--- android/LittleSchemer/app/src/main/java/com/example/chrisetheridge/littleschemer/MenuActivity_v2.java
+++ android/LittleSchemer/app/src/main/java/com/example/chrisetheridge/littleschemer/MenuActivity_v2.java
@@ -1,7 +1,9 @@
 package com.example.chrisetheridge.littleschemer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity_v2 extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MenuActivity_v2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void schemesButtonOnTap(View view) {
+        Intent i = new Intent(this, LittleSchemerMain_v2.class);
+
+        startActivity(i);
+    }
+
+    public void howToButtonOnTap(View view) {
+        Intent i = new Intent(this, ActivityHowTo.class);
+
+        startActivity(i);
     }
 }
